ad5bb9614b84df23ed22193593e7a6cf846e29ae
VladTrusiuk2503 <vladislavtrusiuk25@gmail.com>
2023-02-16 21:38:39 +0200

    email

--- js/custom.js
+++ js/custom.js
@@ -56,72 +56,72 @@ $(window).on('load', function(){
 	/*   Newsletter                                               */
 	/* ========================================================== */
 	
-	$('.newsletter-form').each( function(){
-		var form = $(this);
-		//form.validate();
-		form.submit(function(e) {
-			if (!e.isDefaultPrevented()) {
-				jQuery.post(this.action,{
-					'email':$('input[name="nf_email"]').val(),
-				},function(data){
-					form.fadeOut('fast', function() {
-						$(this).siblings('p.newsletter_success_box').show();
-					});
-				});
-				e.preventDefault();
-			}
-		});
-	});		
+	// $('.newsletter-form').each( function(){
+	// 	var form = $(this);
+	// 	//form.validate();
+	// 	form.submit(function(e) {
+	// 		if (!e.isDefaultPrevented()) {
+	// 			jQuery.post(this.action,{
+	// 				'email':$('input[name="nf_email"]').val(),
+	// 			},function(data){
+	// 				form.fadeOut('fast', function() {
+	// 					$(this).siblings('p.newsletter_success_box').show();
+	// 				});
+	// 			});
+	// 			e.preventDefault();
+	// 		}
+	// 	});
+	// });		
 	
 
 	/* ========================================================== */
 	/*   Register Top Home Section                                */
 	/* ========================================================== */
 	
-	$('#register-form-home').each( function(){
-		var form = $(this);
-		//form.validate();
-		form.submit(function(e) {
-			if (!e.isDefaultPrevented()) {
-				jQuery.post(this.action,{
-					'names':$('input[name="register_names_home"]').val(),
-					'email':$('input[name="register_email_home"]').val(),
-					'ticket':$('select[name="register_ticket_home"]').val(),
-				},function(data){
-					form.fadeOut('fast', function() {
-						$(this).siblings('p.register_success_box_home').show();
-					});
-				});
-				e.preventDefault();
-			}
-		});
-	})		
+	// $('#register-form-home').each( function(){
+	// 	var form = $(this);
+	// 	//form.validate();
+	// 	form.submit(function(e) {
+	// 		if (!e.isDefaultPrevented()) {
+	// 			jQuery.post(this.action,{
+	// 				'names':$('input[name="register_names_home"]').val(),
+	// 				'email':$('input[name="register_email_home"]').val(),
+	// 				'ticket':$('select[name="register_ticket_home"]').val(),
+	// 			},function(data){
+	// 				form.fadeOut('fast', function() {
+	// 					$(this).siblings('p.register_success_box_home').show();
+	// 				});
+	// 			});
+	// 			e.preventDefault();
+	// 		}
+	// 	});
+	// })		
 	
 
 	/* ========================================================== */
 	/*   Contact                                                 */
 	/* ========================================================== */
 	
-	$('#contact-form').each( function(){
-		var form = $(this);
-		//form.validate();
-		form.submit(function(e) {
-			if (!e.isDefaultPrevented()) {
-				jQuery.post(this.action,{
-					'names':$('input[name="contact_names"]').val(),
-					'phone':$('input[name="contact_phone"]').val(),
-					'email':$('input[name="contact_email"]').val(),
-					'ticket':$('select[name="contact_ticket"]').val(),
-					'message':$('textarea[name="contact_message"]').val(),
-				},function(data){
-					form.fadeOut('fast', function() {
-						$(this).siblings('p.contact_success_box').show();
-					});
-				});
-				e.preventDefault();
-			}
-		});
-	})
+	// $('#contact-form').each( function(){
+	// 	var form = $(this);
+	// 	//form.validate();
+	// 	form.submit(function(e) {
+	// 		if (!e.isDefaultPrevented()) {
+	// 			jQuery.post(this.action,{
+	// 				'names':$('input[name="contact_names"]').val(),
+	// 				'phone':$('input[name="contact_phone"]').val(),
+	// 				'email':$('input[name="contact_email"]').val(),
+	// 				'ticket':$('select[name="contact_ticket"]').val(),
+	// 				'message':$('textarea[name="contact_message"]').val(),
+	// 			},function(data){
+	// 				form.fadeOut('fast', function() {
+	// 					$(this).siblings('p.contact_success_box').show();
+	// 				});
+	// 			});
+	// 			e.preventDefault();
+	// 		}
+	// 	});
+	// })
 });
 
 	
@@ -156,4 +156,44 @@ $(window).on('load', function(){
 		  enabled:false
 		}
 	});  
+	$(document).ready(function() {
+
+
+
+		//E-mail Ajax Send
+	  
+		$("form").submit(function() { //Change
+	  
+		  var th = $(this);
+		  	// th.validate();
+	  
+		  $.ajax({
+	  
+			type: "POST",
+	  
+			url: "mail.php", //Change
+	  
+			data: th.serialize()
+	  
+		  }).done(function() {
+	  
+			alert("Message send!");
+	  
+			setTimeout(function() {
+	  
+			  // Done Functions
+	  
+			  th.trigger("reset");
+	  
+			}, 1000);
+	  
+		  });
+	  
+		  return false;
+	  
+		});
+	  
+	  
+	  
+	  });
  
